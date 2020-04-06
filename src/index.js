ba6b7e3412791d06c8d7f6751467b568be9adaad
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-04-06 21:52:19 +0700

    Delete unnecessary lines of code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 import Spinner from "./Spinner";
 import useLocation from "./useLocation";
 
 const App = () => {
-  const [lat, errorMessage] = useLocation();
+	const [lat, errorMessage] = useLocation();
 
-  let content;
-  if (lat) {
-    content = <SeasonDisplay lat={lat} />;
-  } else if (errorMessage) {
-    content = <div>Error: {errorMessage}</div>;
-  } else {
-    content = <Spinner message="Please accept location request" />;
-  }
+	let content;
+	if (lat) {
+		content = <SeasonDisplay lat={lat} />;
+	} else if (errorMessage) {
+		content = <div>Error: {errorMessage}</div>;
+	} else {
+		content = <Spinner message="Please accept location request" />;
+	}
 
-  return <div className="border red">{content}</div>;
+	return <div className="border red">{content}</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
